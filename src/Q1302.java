fa1bf232811eed9f5b5fb5aa339db561031b2976
PeonyF <1621139@gmail.com>
2020-01-07 00:58:09 +0900

    update BackJoon's Algorithms

---
+++ src/Q1302.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Q1302 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BestSeller bestSeller = new BestSeller();
+        int countSoldBooks = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < countSoldBooks; i++) {
+            String bookName = br.readLine();
+            bestSeller.addBook(bookName);
+        }
+
+        String bestSellerBook = bestSeller.getBookName();
+        System.out.println(bestSellerBook);
+
+    }
+}
+
+class BestSeller {
+    private static final int baseBook = 1;
+
+    private Map<String, Integer> bookMap = new HashMap<>();
+    private int value;
+    private String bestSellerBook = "";
+    private int max = 0;
+
+    void addBook(String bookName) {
+        if (checkContainsBook(bookName)) {
+            int countBook = bookMap.get(bookName);
+            bookMap.put(bookName, countBook + baseBook);
+        }
+        if (!checkContainsBook(bookName)) {
+            bookMap.put(bookName, baseBook);
+        }
+    }
+
+    private boolean checkContainsBook(String bookName) {
+
+        return bookMap.containsKey(bookName);
+    }
+
+    String getBookName() {
+        for (String key : bookMap.keySet()) {
+            value = bookMap.get(key);
+            toFindMaxNumOfBooks(key);
+        }
+        return bestSellerBook;
+    }
+
+    private void toFindMaxNumOfBooks(String key) {
+        if (max == value && bestSellerBook.compareTo(key) > 0) {
+            bestSellerBook = key;
+            max = value;
+        }
+        if (max < value) {
+            bestSellerBook = key;
+            max = value;
+        }
+    }
+
+}
