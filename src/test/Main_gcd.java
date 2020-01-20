bb560b0ea8ba95719223d8dee1b7c8c9a4bcabfa
PeonyF <1621139@gmail.com>
2020-01-20 18:00:37 +0900

    update BackJoon's Algorithm Q3036.

--- src/test/Main_gcd.java
+++ src/test/Main_gcd.java
@@ -4,33 +4,33 @@ import java.util.Scanner;
 
 public class Main_gcd {
     public static void main(String[] args) {
+        int val1, val2;
+        int r = 1;
+        int LCM;
+
         Scanner sc = new Scanner(System.in);
-        CommonMeasure commonMeasure = new CommonMeasure();
 
+        System.out.print("첫번째 수를 입력하시오 : ");
+        val1 = sc.nextInt();
+        System.out.print("두번째 수를 입력하시오 : ");
+        val2 = sc.nextInt();
 
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        System.out.println(commonMeasure.gcd(num1, num2));
-        System.out.println(commonMeasure.lcm(num1, num2));
-    }
-
-}
-
-
-class CommonMeasure {
-    int gcd(int num1, int num2) {
-        while (num2 > 0) {
-            int temp = num2;
-            num2 = num1 % num2;
-            num1 = temp;
+        int tmp1 = val1, tmp2 = val2;//최소공배수를 구하기 위해 입력받은 값 저장
+        if (val1 < val2) {//2번째 입력수가 클 경우 큰수를 val1으로 변경
+            val1 = tmp2;
+            val2 = tmp1;
+            System.out.println(val1+", "+val2);
         }
-        return num1;
-    }
 
+        while (r > 0) {//유클리드 호제법을 이용한 GCD(최대공약수)구하기
+            System.out.println(val1+", "+val2);
+            r = val1 % val2;
+            val1 = val2;
+            val2 = r;
+        }
+        LCM = tmp1 * tmp2 / val1;//LCM(최소공배수)구하기
+        System.out.println("GCD(최대공약수) : " + val1);
+        System.out.println("LCM(최소공배수) : " + LCM);
 
-    int lcm(int num1, int num2) {
-
-        return (num1 * num2) / gcd(num1, num2);
     }
 }
