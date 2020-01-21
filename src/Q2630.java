51571d5714f9ebcdbd4b94e40ffdaf403ed46c7c
PeonyF <1621139@gmail.com>
2020-01-21 23:36:10 +0900

    update 2 solutions and change the variables in Q2630.

--- src/Q2630.java
+++ src/Q2630.java
@@ -3,36 +3,39 @@ import java.util.Scanner;
 public class Q2630 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
+        int maxSize = sc.nextInt();
 
-        int[][] arr = new int[size][size];
+        int[][] inputArr = new int[maxSize][maxSize];
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                arr[i][j] = sc.nextInt();
+        for (int i = 0; i < maxSize; i++) {
+            for (int j = 0; j < maxSize; j++) {
+                inputArr[i][j] = sc.nextInt();
             }
         }
 
-        ColorPaper colorPaper = new ColorPaper(arr);
-        colorPaper.toFindColor(0, 0, size);
+        ColorPaper colorPaper = new ColorPaper(inputArr);
+        colorPaper.toFindColor(0, 0, maxSize);
         System.out.println(colorPaper.printColor());
     }
 }
 
 class ColorPaper {
-    private static final int divide = 2;
-    private int[][] input;
+    private static final int divideTwo = 2;
+    private static final int white = 0;
+    private static final int blue = 1;
 
-    ColorPaper(int[][] arr) {
-        this.input = arr;
+    private int[][] inputArr;
+
+    ColorPaper(int[][] inputArr) {
+        this.inputArr = inputArr;
     }
 
     private boolean flagToFindSameColor(int row, int col, int size) {
-        int cur = input[row][col];
+        int compareElement = inputArr[row][col];
 
         for (int i = row; i < row + size; i++) {
             for (int j = col; j < col + size; j++) {
-                if (cur != input[i][j]) {
+                if (compareElement != inputArr[i][j]) {
                     return false;
                 }
             }
@@ -40,32 +43,42 @@ class ColorPaper {
         return true;
     }
 
-    private int blue = 0;
-    private int white = 0;
+    private int countOfBlue = 0;
+    private int countOfWhite = 0;
 
     void toFindColor(int row, int col, int size) {
         if (flagToFindSameColor(row, col, size)) {
             configureColor(row, col);
         } else {
-            int divideSize = size / divide;
-            toFindColor(row, col, divideSize);
-            toFindColor(row, col + divideSize, divideSize);
-            toFindColor(row + divideSize, col, divideSize);
-            toFindColor(row + divideSize, col + divideSize, divideSize);
+            int divideSize = size / divideTwo;
+
+            //  방법1
+//            toFindColor(row, col, divideSize);
+//            toFindColor(row, col + divideSize, divideSize);
+//            toFindColor(row + divideSize, col, divideSize);
+//            toFindColor(row + divideSize, col + divideSize, divideSize);
+
+            // 방법2
+
+            for(int i=0; i<divideTwo; i++){
+                for(int j=0; j<divideTwo; j++){
+                    toFindColor(row+divideSize*i, col+divideSize*j,divideSize);
+                }
+            }
         }
     }
 
     private void configureColor(int row, int col) {
-        if (input[row][col] == 1) {
-            blue++;
+        if (inputArr[row][col] == blue) {
+            countOfBlue++;
         }
-        if (input[row][col] == 0) {
-            white++;
+        if (inputArr[row][col] == white) {
+            countOfWhite++;
         }
     }
 
     String printColor() {
-        return white + "\n" + blue;
+        return countOfWhite + "\n" + countOfBlue;
     }
 
 }
