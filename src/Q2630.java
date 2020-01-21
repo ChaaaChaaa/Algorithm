3c84bb073e0ab178b9f3aa1486efcf31723a0f73
PeonyF <1621139@gmail.com>
2020-01-21 22:05:00 +0900

    update BackJoon's Algorithm Q2630.

---
+++ src/Q2630.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Q2630 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int matrixSize = Integer.parseInt(br.readLine().trim());
+        int[][] input = new int[matrixSize][matrixSize];
+
+        for (int i = 0; i < matrixSize; i++) {
+            char[] line = br.readLine().toCharArray();
+            for (int j = 0; j < line.length; j++) {
+                input[i][j] = line[j] - '0';
+            }
+        }
+        ColoredPaper coloredPaper = new ColoredPaper(input);
+        coloredPaper.toFindQuadTree(0, 0, matrixSize);
+        System.out.println(coloredPaper.printQuadTree());
+    }
+}
+
+class ColoredPaper {
+    private int[][] input;
+
+    ColoredPaper(int[][] input) {
+        this.input = input;
+    }
+
+    private boolean flagToFindSameMatrix(int row, int col, int size) {
+        int cur = input[row][col];
+        for (int i = row; (i < row + size); i++) {
+            for (int j = col; (j < col + size); j++) {
+
+                if (cur != input[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private StringBuilder sb = new StringBuilder();
+
+    void toFindQuadTree(int row, int col, int size) {
+        if (flagToFindSameMatrix(row, col, size)) {
+            sb.append(input[row][col]);
+        }
+
+
+        else {
+            sb.append("(");
+            int divide = size / 2;
+            toFindQuadTree(row, col, divide);
+            toFindQuadTree(row, col + divide, divide);
+            toFindQuadTree(row + divide, col, divide);
+            toFindQuadTree(row + divide, col + divide, divide);
+            sb.append(")");
+        }
+    }
+
+    String printQuadTree() {
+        return sb.toString();
+    }
+
+}
