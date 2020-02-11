6fd30f6855fbece041c8a2550f86743752eef95f
PeonyF <1621139@gmail.com>
2020-02-11 15:29:27 +0900

    update Q1152.

---
+++ src/Q1152.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sentence = br.readLine();
+
+        NumOfWords numOfWords = new NumOfWords(sentence);
+
+        numOfWords.deleteSpaces();
+        numOfWords.makeArrSplitSpace();
+        numOfWords.printCount();
+    }
+}
+
+class NumOfWords {
+    private String sentence;
+    private String[] strArr;
+    private String convertSpacesToOne;
+
+    NumOfWords(String sentence) {
+        this.sentence = sentence;
+    }
+
+    void deleteSpaces() {
+        String removeSpace = sentence.trim();
+        convertSpacesToOne = removeSpace.replace("\\p{Z}", " ");
+        strArr = convertSpacesToOne.split(" ");
+    }
+
+    void makeArrSplitSpace() {
+        strArr = convertSpacesToOne.split(" ");
+    }
+
+    private boolean checkBlankWord() {
+        return strArr[0].length() == 0;
+    }
+
+    void printCount() {
+        int count = strArr.length;
+
+        if (checkBlankWord()) {
+            System.out.println(count - 1);
+            return;
+        }
+        System.out.println(count);
+    }
+
+}
