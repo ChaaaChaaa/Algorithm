68b28339a678622a78a6aa4d3badd73ef86a1c6a
PeonyF <1621139@gmail.com>
2020-05-20 15:37:57 +0900

    [ADD] Q401

--- src/leetcode/Q401.java
+++ src/leetcode/Q401.java
@@ -3,17 +3,48 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Q784 {
+public class Q401 {
+    private static final int[] watch = {1, 2, 4, 8, 1, 2, 4, 8, 16, 32};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(readBinaryWatch(num));
     }
 
-    public 
+    private static List<String> readBinaryWatch(int num) {
+        List<String> list = new ArrayList<>();
 
-    public List<String> letterCasePermutation(String s){
+        if (num >= 0) {
+            recursion(list, num, 0, 0, 0);
+        }
 
+        return list;
     }
+
+    private static void recursion(List<String> list, int num, int hour, int minute, int start) {
+        if (num <= 0) {
+            if (hour < 12 && minute < 60) {
+                if (minute < 10) {
+                    list.add(hour + ":0" + minute);
+                } else {
+                    list.add(hour + ":" + minute);
+                }
+            }
+        } else {
+            for (int i = start; i < watch.length; i++) {
+                if (i < 4) {
+                    recursion(list, num - 1, hour + watch[i], minute, i + 1);
+                } else {
+                    recursion(list, num - 1, hour, minute + watch[i], i + 1);
+                }
+            }
+        }
+    }
+
+
 }
+
