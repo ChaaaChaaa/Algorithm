f1251d30fd532b9573d55804dc19a976a718b52f
PeonyF <1621139@gmail.com>
2020-05-26 21:52:59 +0900

    [UPDATE[ Q14425

--- src/boj/Q14425.java
+++ src/boj/Q14425.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Q14425 {
