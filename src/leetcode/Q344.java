62ddb482121566edf53664741954f9fce2a523c5
PeonyF <1621139@gmail.com>
2020-06-30 22:08:37 +0900

    [ADD] Hashmap ListOfPhoneNumber

---
+++ src/leetcode/Q344.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class Q21 {
+    public static void main(String[] args){
+        ListNode<>()
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    }
+}
