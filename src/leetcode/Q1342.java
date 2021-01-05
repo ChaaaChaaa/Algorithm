9e74fa1d8965e1696579a6385cde77fb0418049c
MJ Choi <1621139@gmail.com>
2021-01-05 22:21:19 +0900

    [ADD] Q1342 leetcode

---
+++ src/leetcode/Q1342.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class Q1342 {
+    public static void main(String[] args) {
+        int num = 14;
+        System.out.println(numberOfSteps(num));
+    }
+
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
