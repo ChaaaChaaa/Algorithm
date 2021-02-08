c0ffc77cdbb90a45d02f69ca86cf29bb623bf40e
MJ Choi <1621139@gmail.com>
2021-02-08 23:53:23 +0900

    [ADD] Q39 leetcode

---
+++ src/leetcode/Q39.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q39 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 6, 7};
+        int target = 7;
+        System.out.println(combinationSum(nums, target));
+    }
+
+    public static List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backTrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private static void backTrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            System.out.println(list);
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                tempList.add(nums[i]);
+                backTrack(list, tempList, nums, remain - nums[i], i);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
